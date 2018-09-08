80c5e1a017745461881a222a46e9ab470f1025fa
Blair Thurman <blairdev1023@gmail.com>
2018-09-08 14:07:07 -0700

    adding main layout

--- app.py
+++ app.py
@@ -12,73 +12,89 @@ import plotly.graph_objs as go
 app = dash.Dash()
 server = app.server
 
-df_state = pd.read_csv('data/state_clean.csv')
+df_gv = pd.read_csv('data/gun_violence_clean.csv')
+df_state = pd.read_csv('data/state_clean.csv', index_col='name')
+
+mapbox_access_token = os.environ['MAPBOX_ACCESS_TOKEN']
+
+def lookup(state, feature, years=range(2014, 2018)):
+    return [df_state.loc[state, f'{feature}{year}'] for year in years]
 
 app.layout = html.Div([
-    # App Title
     html.H1('U.S. Gun Violence 2014-2018', style={'textAlign': 'center'}),
-    # YoY
+    # State Choropleth
     html.Div([
-        html.H2('U.S. Yearly Totals', style={'textAlign': 'center'}),
-        # Selectors
-        html.Div([
-            # Metric Radio
-            html.Div(
-                dcc.RadioItems(
-                    id='yoy-radio-metric',
-                    options=[{'label': i, 'value': i} for i in
-                        ['Raw', 'Per 100,000']],
-                    value='Raw'
-                ),
-            style={'margin-left': 'auto', 'margin-right': 'auto'}
-            ),
-            # Feature Dropdwon
-            html.Div(
-                dcc.Dropdown(
-                    id='yoy-dropdown-feature',
-                    options=[{'label': i, 'value': i} for i in
-                        ['Killed', 'Injured', 'Total']],
-                    value='Killed'
-                ),
-            style={'margin-left': 'auto', 'margin-right': 'auto'}
-            )
-        ],
-        style={'width': '25%', 'margin-left': 'auto', 'margin-right': 'auto'}
-        ),
+        html.H2('Incidents by State', style={'textAlign': 'center'}),
         # Main Plot
-        dcc.Graph(id='yoy-plot')
-    ],
-    style={'width': '85%', 'margin-left': 'auto', 'margin-right': 'auto'}
-    ),
-    # All States
-    html.Div([
-        html.H2('Gun Violence by State', style={'textAlign': 'center'}),
+        html.Div([
+            dcc.Graph(id='choropleth-plot', config={'displayModeBar': False})
+        ], style={'width': '70%', 'display': 'inline-block'}),
+        # Side Plot and Totals Info
+        html.Div([
+            dcc.Graph(id='choropleth-trend', config={'displayModeBar': False}),
+            dcc.Textarea(
+                id='choropleth-totals',
+                readOnly=True,
+                wrap=True,
+                style={'width': '100%', 'height': 250}
+            )
+        ], style={'width': '30%', 'height': 500, 'display': 'inline-block'}),
+        # Selectors
+        # Year Slider
+        html.Div(
+            dcc.Slider(
+                id='choropleth-slider-year',
+                min=2014,
+                max=2017,
+                step=1,
+                value=2017,
+                marks={str(i) : str(i) for i in range(2014, 2018)}
+            ),
+        style={'width': '40%',  'display': 'inline-block', 'margin-left': '10%'}
+        ),
         # Feature Dropdwon
         html.Div(
             dcc.Dropdown(
-                id='state-dropdown-feature',
+                id='choropleth-dropdown-feature',
                 options=[{'label': i, 'value': i} for i in
                     ['Killed', 'Injured', 'Total']],
+                multi=True,
                 value='Killed'
             ),
-        style={'width': '30%', 'margin-left': '70%'}
+        style={'width': '30%', 'display': 'inline-block', 'float': 'right'}
         ),
-        # Main Plot
-        dcc.Graph(id='state-plot')
-    ],
-    style={'width': '85%', 'margin-left': 'auto', 'margin-right': 'auto'}
-    ),
+    ]),
     # Individual Incidents
     html.Div([
         html.H2('Individual Incidents', style={'textAlign': 'center'}),
+        # Main Plot
+        html.Div([
+            dcc.Graph(id='incident-plot'),
+        ], style={'width': '70%', 'display': 'inline-block'}),
+        # Info Boxes
+        html.Div([
+            html.H3('Info'),
+            dcc.Textarea(
+                id='incident-info',
+                readOnly=True,
+                wrap=True,
+                style={'width': '100%', 'height': 180}
+            ),
+            html.H3('Notes'),
+            dcc.Textarea(
+                id='incident-notes',
+                readOnly=True,
+                wrap=True,
+                style={'width': '100%', 'height': 180}
+            )
+        ], style={'width': '30%', 'height': 500, 'display': 'inline-block'}),
         # Selectors
         html.Div([
             # State Dropdown
             html.Div(
                 dcc.Dropdown(
-                    id='individual-dropdown-state',
-                    options=[{'label': i, 'value': i} for i in
-                        np.unique(df_state['name'])],
+                    id='incident-dropdown-state',
+                    options=[{'label': i, 'value': i} for i in df_state.index],
                     value='Nevada',
                 ),
             style={'width': '50%', 'display': 'inline-block'}
@@ -86,9 +102,9 @@ app.layout = html.Div([
             # Year Checklist
             html.Div(
                 dcc.Checklist(
-                    id='individual-checklist-year',
+                    id='incident-checklist-year',
                     options=[{'label': i, 'value': i} for i in
-                        [2014, 2015, 2016, 2017, 2018]],
+                        range(2014, 2019)],
                     values=[2018]
                 ),
             style={'width': '50%', 'float': 'right'}
@@ -96,14 +112,124 @@ app.layout = html.Div([
         ],
         style={'width': '50%', 'margin-left': 'auto', 'margin-right': 'auto'}
         ),
-        # Main Plot
-        dcc.Graph(id='individual-plot')
-    ],
-    style={'width': '85%', 'margin-left': 'auto', 'margin-right': 'auto'}
+    ])
+],
+style={
+    'width': '85vw',
+    'margin-left': 'auto',
+    'margin-right': 'auto'
+}
+)
+
+app.css.append_css({
+    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
+
+@app.callback(
+    Output('choropleth-plot', 'figure'),
+    [Input('choropleth-dropdown-feature', 'value')])
+def choropleth_plot(feature):
+    '''
+    Returns a plotly figure for the main state choropleth
+    '''
+    data = [{
+        'type': 'choropleth',
+        'locationmode': 'USA-states',
+    }]
+
+    layout = go.Layout(
+        margin={'l': 10, 'b': 20, 't': 0, 'r': 10},
+        geo = dict(
+            scope='usa',
+            projection=dict(type='albers usa'),
+        ),
     )
-])
+
+    return {'data': data, 'layout': layout}
+
+
+@app.callback(
+    Output('choropleth-trend', 'figure'),
+    [Input('choropleth-plot', 'hoverData')])
+def choropleth_trend(hoverData):
+    '''
+    This function will return a plotly figure
+
+    This figure will have a trace for each requested feature aggregated by state
+    '''
+    data = []
+    layout = go.Layout(
+        height=250,
+        margin={'l': 20, 'b': 20, 't': 5, 'r': 5},
+    )
+    return {'data': data, 'layout': layout}
+
+@app.callback(
+    Output('choropleth-totals', 'value'),
+    [Input('choropleth-plot', 'hoverData')])
+def choropleth_totals(hoverData):
+    '''
+    This function will return the totals info for the hover-on state
+    '''
+    return '''Meow
+Cats Are Cute, the cutest animals in all the world, is this wrapping?
+Cats are kewl
+    '''
+
+
+@app.callback(
+    Output('incident-plot', 'figure'),
+    [Input('incident-dropdown-state', 'value'),
+    Input('incident-checklist-year', 'values')])
+def incident_plot(state, year):
+    '''
+    Returns figure for the main Mapbox
+    '''
+    data = [go.Scattermapbox(
+        lat=[39.73, 39.74],
+        lon=[-104.99, -105.00],
+        mode='markers',
+        marker={'size': 9, 'color': 'black'},
+        text='Denver'
+    )]
+
+    layout = go.Layout(
+        margin={'l': 10, 'b': 20, 't': 0, 'r': 10},
+        autosize=True,
+        mapbox=dict(
+            accesstoken=mapbox_access_token,
+            center={'lat': 39.73, 'lon': -104.99},
+            zoom=10
+        )
+    )
+
+    return {'data': data, 'layout': layout}
+
+@app.callback(
+    Output('incident-info', 'value'),
+    [Input('incident-plot', 'hoverData')])
+def incident_info(hoverData):
+    '''
+    This will be a function that returns the basic info for each marker
+
+    Right now it doesn't work until the user hovers over a marker
+    '''
+    killed = 'Killed: 5'
+    injured = 'Injured: 13'
+    guntype = 'Guntype: Pistol'
+    hover_text = str(hoverData['points'][0]['lat'])
+    print(hover_text)
+    text = '\n'.join([killed, injured, guntype, hover_text])
+    return text
+
+@app.callback(
+    Output('incident-notes', 'value'),
+    [Input('incident-plot', 'hoverData')])
+def incident_notes(hoverData):
+    '''
+    This will be a function for returning the notes of the incident
+    '''
+    return 'Someone shot some people'
 
 if __name__ == '__main__':
-    app.scripts.config.serve_locally = True
-    app.css.config.serve_locally = True
     app.run_server(debug=True)
