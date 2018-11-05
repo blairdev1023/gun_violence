bdc9c38df4d2141778012dd457662d4755cdfe83
Blair Thurman <blairdev1023@gmail.com>
2018-11-05 16:33:16 -0700

    adding recursive error handling for SSLError

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,6 +5,18 @@ import time
 import pandas as pd
 import numpy as np
 
+def scrape(idx):
+    '''
+    returns the soup form the url
+    '''
+    url = f'https://www.gunviolencearchive.org/incident/{idx}'
+    try:
+        page = requests.get(url)
+        return BeautifulSoup(page.text, 'html.parser')
+    except requests.exceptions.SSLError:
+        print(f'SSLError on {idx}, trying again...', time.time() - start)
+        return scrape(idx)
+
 def check_idxs(lower_bound, upper_bound):
     '''
     returns a list of the incident indices between the bounds
@@ -13,10 +25,8 @@ def check_idxs(lower_bound, upper_bound):
     for idx in range(lower_bound, upper_bound):
         if idx % 100 == 0:
             time.sleep(1)
-            print(idx - lower_bound, len(page_idxs))
-        url = f'https://www.gunviolencearchive.org/incident/{idx}'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.text, 'html.parser')
+            print(idx - lower_bound, len(page_idxs), time.time() - start)
+        soup = scrape(idx)
         if soup.h1.text != '\nPage not found\n':
             page_idxs.append(idx)
     return page_idxs
@@ -24,4 +34,4 @@ def check_idxs(lower_bound, upper_bound):
 if __name__ == '__main__':
     start = time.time()
     page_idxs = check_idxs(130000, 140000)
-    print(time.time() - start)
+    print(len(page_idxs), time.time() - start)
